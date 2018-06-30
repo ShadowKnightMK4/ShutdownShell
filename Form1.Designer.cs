cf52bf5e6947a2dd824b4c21ba2721a5a40c7150
ShadowKnightMK4 <37078953+ShadowKnightMK4@users.noreply.github.com>
2018-06-29 20:43:12 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ShutdownShell
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.LogOffButton = new System.Windows.Forms.Button();
+			this.HibernateButton = new System.Windows.Forms.Button();
+			this.ShutdownButton = new System.Windows.Forms.Button();
+			this.CheckBoxTopMost = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// LogOffButton
+			// 
+			this.LogOffButton.Location = new System.Drawing.Point(12, 12);
+			this.LogOffButton.Name = "LogOffButton";
+			this.LogOffButton.Size = new System.Drawing.Size(90, 36);
+			this.LogOffButton.TabIndex = 0;
+			this.LogOffButton.Text = "Log Off";
+			this.LogOffButton.UseVisualStyleBackColor = true;
+			this.LogOffButton.Click += new System.EventHandler(this.LogOff_ButtonClick);
+			// 
+			// HibernateButton
+			// 
+			this.HibernateButton.Location = new System.Drawing.Point(12, 54);
+			this.HibernateButton.Name = "HibernateButton";
+			this.HibernateButton.Size = new System.Drawing.Size(90, 35);
+			this.HibernateButton.TabIndex = 1;
+			this.HibernateButton.Text = "Hibernate";
+			this.HibernateButton.UseVisualStyleBackColor = true;
+			this.HibernateButton.Click += new System.EventHandler(this.HibernateButton_Click);
+			// 
+			// ShutdownButton
+			// 
+			this.ShutdownButton.Location = new System.Drawing.Point(12, 95);
+			this.ShutdownButton.Name = "ShutdownButton";
+			this.ShutdownButton.Size = new System.Drawing.Size(90, 35);
+			this.ShutdownButton.TabIndex = 2;
+			this.ShutdownButton.Text = "Shutdown";
+			this.ShutdownButton.UseVisualStyleBackColor = true;
+			this.ShutdownButton.Click += new System.EventHandler(this.ShutdownButton_Click);
+			// 
+			// CheckBoxTopMost
+			// 
+			this.CheckBoxTopMost.AutoSize = true;
+			this.CheckBoxTopMost.Location = new System.Drawing.Point(109, 12);
+			this.CheckBoxTopMost.Name = "CheckBoxTopMost";
+			this.CheckBoxTopMost.Size = new System.Drawing.Size(97, 24);
+			this.CheckBoxTopMost.TabIndex = 3;
+			this.CheckBoxTopMost.Text = "Topmost";
+			this.CheckBoxTopMost.UseVisualStyleBackColor = true;
+			this.CheckBoxTopMost.CheckedChanged += new System.EventHandler(this.CheckBoxTopMost_Clicked);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(423, 142);
+			this.Controls.Add(this.CheckBoxTopMost);
+			this.Controls.Add(this.ShutdownButton);
+			this.Controls.Add(this.HibernateButton);
+			this.Controls.Add(this.LogOffButton);
+			this.Name = "MainForm";
+			this.Text = "Shutdown Shell Menu";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button LogOffButton;
+		private System.Windows.Forms.Button HibernateButton;
+		private System.Windows.Forms.Button ShutdownButton;
+		private System.Windows.Forms.CheckBox CheckBoxTopMost;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+[assembly: CLSCompliant(false)]
+namespace ShutdownShell
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		
+		private const byte CfgTrue_Val = 60;
+		private const byte CfgFalse_Val = 30;
+
+		enum Choice
+		{
+			LogOff = 1,
+			Hibernate = 2,
+			Shutdown = 3,
+
+
+		}
+
+
+		// make the cfg file path in the user's app data directory
+		private static string BuildCfgLocation()
+		{
+			StringBuilder loc = new StringBuilder();
+			loc.Append(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
+			if (loc[loc.Length - 1] != '\\')
+			{
+				loc.Append('\\');
+			}
+
+			loc.Append("ShutdownShellApp.cfg");
+			return loc.ToString();
+
+		}
+
+		/// <summary>
+		/// assemble the path to the shutdown app in the windows sytem folder
+		/// </summary>
+		/// <returns></returns>
+		private static string BuildShutdownAppLocation()
+		{
+			StringBuilder name = new StringBuilder();
+
+			name.Append("\"");
+			name.Append(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86));
+
+			if (name[name.Length-1]  != '\\')
+			{
+				name.Append('\\');
+			}
+
+			name.Append("shutdown.exe");
+			name.Append("\"");
+			return name.ToString();
+		}
+		private static void HandleShutdown(Choice c)
+		{
+			Process shutdown = new Process();
+			ProcessStartInfo info = new ProcessStartInfo
+			{
+				FileName = BuildShutdownAppLocation(),
+				WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.SystemX86)
+			};
+
+
+			switch (c)
+			{
+				case Choice.LogOff:
+					info.Arguments = "/l";
+					break;
+				case Choice.Shutdown:
+					info.Arguments = "/s";
+					break;
+				case Choice.Hibernate:
+					info.Arguments = "/h";
+					break;
+			}
+
+			info.CreateNoWindow = false;
+			shutdown.StartInfo = info;
+			shutdown.StartInfo.UseShellExecute = true;
+			if (info.Arguments != null)
+			{
+				shutdown.Start();		
+			}
+		}
+
+
+		private void LogOff_ButtonClick(object sender, EventArgs e)
+		{
+			HandleShutdown(Choice.LogOff);
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			String cfgLoc;
+			var ts = Size;
+			TopMost = true;
+			MaximizeBox = false;
+			ShowInTaskbar = true;
+			MinimizeBox = true;
+
+			cfgLoc = BuildCfgLocation();
+			
+			
+				
+				
+				try
+				{
+					using (FileStream fn = new FileStream(cfgLoc.ToString(), FileMode.Open))
+					{
+						if (fn.ReadByte() == CfgTrue_Val)
+						{
+							CheckBoxTopMost.Checked = true;
+							TopMost = true;
+						}
+						else
+						{
+							CheckBoxTopMost.Checked = false;
+							TopMost = false;
+						}
+					}
+				}
+				catch (IOException)
+				{
+					MessageBox.Show(String.Format("Caution: Unable to open {0}. Some settings not preserved", cfgLoc.ToString()));
+				}
+			
+
+
+		}
+
+		private void Form1_LocationChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+		private void LogOffUser_Click(object sender, EventArgs e)
+		{
+			HandleShutdown(Choice.LogOff);
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+		private void ShutdownTheComputer_Click(object sender, EventArgs e)
+		{
+			var answer = MessageBox.Show("Are you sure you wish to shutdown?", "Shutdown?", MessageBoxButtons.YesNo);
+			if (answer == DialogResult.Yes)
+				HandleShutdown(Choice.Shutdown);
+
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+		private void HibernatePc_Click(object sender, EventArgs e)
+		{
+			HandleShutdown(Choice.Hibernate);
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+		private void AkwaysOnTopToggle_Click(object sender, EventArgs e)
+		{
+			CheckBox self = (CheckBox)sender;
+			self.Checked = (self.Checked != true);
+
+			TopMost = self.Checked;
+		}
+
+
+
+		private void HibernateButton_Click(object sender, EventArgs e)
+		{
+			HandleShutdown(Choice.Hibernate);
+		}
+
+		private void ShutdownButton_Click(object sender, EventArgs e)
+		{
+			HandleShutdown(Choice.Shutdown);
+		}
+
+		private void CheckBoxTopMost_Clicked(object sender, EventArgs e)
+		{
+			TopMost = ((CheckBox)sender).Checked;
+		}
+
+		private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			String cfgLoc = BuildCfgLocation();
+			try
+			{
+				using (FileStream fn = new FileStream(cfgLoc, FileMode.OpenOrCreate))
+				{
+					if (CheckBoxTopMost.Checked == true)
+					{
+						fn.WriteByte(CfgTrue_Val);
+					}
+					else
+					{
+						fn.WriteByte(CfgFalse_Val);
+					}
+				}
+			}
+			catch (IOException)
+			{
+				MessageBox.Show(String.Format("Caution: Unable to open {0}. Some settings not preserved", cfgLoc.ToString()));
+			}
+		}
+	}
+}
